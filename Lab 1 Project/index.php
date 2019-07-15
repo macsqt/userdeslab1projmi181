71b5cbd6a34b8eea74c0b241687503e87f9a02b3
Mark Joshua Macatangay <52182499+macsqt@users.noreply.github.com>
2019-07-16 00:13:29 +0800

    Add files via upload

---
+++ Lab 1 Project/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}
+body, html {
+  height: 100%;
+  color:   #00000 ;
+  line-height: 1.8;
+}
+
+/* Create a Parallax Effect */
+.bgimg-1, .bgimg-2, .bgimg-3 {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+/* First image (Logo. Full height) */
+.bgimg-1 {
+  <img src="image1.jpg">;
+  min-height: 100%;
+}
+.w3-wide {letter-spacing: 10px;}
+.w3-hover-opacity {cursor: pointer;}
+
+/* Turn off parallax scrolling for tablets and phones */
+@media only screen and (max-device-width: 1600px) {
+  .bgimg-1, .bgimg-2, .bgimg-3 {
+    background-attachment: scroll;
+    min-height: 900px;
+  }
+}
+</style>
+<body>
+
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar" id="myNavbar">
+    <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+    </a>
+    <a href="#home" class="w3-bar-item w3-button">HOME</a>
+    <a href="#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
+      <i class="fa fa-search"></i>
+    </a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+    <a href="#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
+    <a href="#portfolio" class="w3-bar-item w3-button" onclick="toggleFunction()">PORTFOLIO</a>
+    <a href="#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+    <a href="#" class="w3-bar-item w3-button">SEARCH</a>
+  </div>
+</div>
+
+
+<!-- First Parallax Image with Logo Text -->
+<div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+<img src="image1.jpg"
+  <div class="w3-display-middle" style="white-space:nowrap;">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">MY <span class="w3-hide-small">WEBSITE</span> LOGO</span>
+  </div>
+</div>
+
+<!-- Container (About Section) -->
+<div class="w3-content w3-container w3-padding-64" id="about">
+  <h3 class="w3-center">______________________________________________________________________________________________________</h3>
+  <h4 class="w3-center">ABOUT ME</h4>
+  <p class="w3-center"><em>I Appreciate Your Existence</em></p>
+  <p>Hi, nice to meet you. I'm Mark Joshua De Villa Macatangay. 
+You can call me Mark. I'm 18 years old and I live in Taguig City. </p>
+<p>How do you do? Well I'm feeling so good today like I could do everything.</p>
+  <div class="w3-row">
+    <div class="w3-col m6 w3-center w3-padding-large">
+      <p><b><i class="fa fa-user w3-margin-right"></i>Mark Joshua D.V. Macatangay</b></p><br>
+      <img src="image2.jpg" class="w3-round w3-image w3-opacity w3-hover-opacity-off" alt="Photo of Me" width="500" height="333">
+    </div>
+
+    <!-- Hide this text on small devices -->
+    <div class="w3-col m6 w3-hide-small w3-padding-large">
+      <p>Welcome to my website. Before I tell my stories, I want to describe myself first. I'm an imperfect person trying to have a perfect life in this imperfect world. It seems impossible but yes, it is impossible.
+	 I'm not that tall, smart, and good looking guy but, you can count on me as a friend. I will be there for you whenever you need a hand. I like to travel even if I'm on my own and I love to seek for an adventure and also to have some fun.
+	 I'm an ambivert person, where I balance my introvert and extrovert personality. Which other people misinterpret it as a two-faced people. I'm an athletic, energetic and hyper person. I love to play volleyball than basketball.
+	 About my academics, IT was never my first choice. I want to be a lawyer and/or a professor someday but my parents didn't approve it. I want to take BA history as my pre-law course but my parents told me that I could not earn a lot in that path. 
+	 I chose IT because I don't want to pursue engineering as my parents told me. It was a like a safe choice but girl, I was wrong. But it's okay, maybe God really put me in this position for his bigger plans.
+	 My very goal in this lifetime as a human being is to just be genuinely happy. I don't care if I will die without a lot of money as long as I'm happy and contented.</p>
+    </div>
+  </div>
+  </div>
+</div>
+
+<div class="w3-row w3-center w3-dark-grey w3-padding-16">
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">14+</span><br>
+    Partners
+  </div>
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">55+</span><br>
+    Projects Done
+  </div>
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">89+</span><br>
+    Happy Clients
+  </div>
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">150+</span><br>
+    Meetings
+  </div>
+</div>
